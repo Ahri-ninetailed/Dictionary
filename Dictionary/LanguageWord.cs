a36900fb60d1ca9a93498ec3698088f5c6cdda4d
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-02-21 23:39:21 +0300

    Определил класс контекста данных и классы сущностей
    Классы сущностей унаследовал от абстрактного класса, чтоб не дублировать
    код

---
+++ Dictionary/LanguageWord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dictionary
+{
+    public abstract class LanguageWord
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Word { get; set; }
+        public List<LanguageWord> OtherWords { get; set; } = new List<LanguageWord>();
+    }
+}
