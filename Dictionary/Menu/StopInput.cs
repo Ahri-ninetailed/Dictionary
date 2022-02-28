ea5979caf03bbc293844cd51198dd0dd3af3f21f
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-02-28 22:11:55 +0300

    Добавил класс StopInput и обновил некоторые классы
    Класс StopInput содержит метод Terms, который принимает делегат типа
    void Action(). В методе строка InputString принимает ввод
    пользователя, ввод в консоль будет доступен до тех пор, пока
    пользователь не введет "Exit" в любом регистре или не нажмет Enter.
    
    Обновил класс AddWords и DeleteWords с помощью метода Terms класса
    StopInput.

---
+++ Dictionary/Menu/StopInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary.Menu
+{
+    class StopInput
+    {
+        public static string InputString { get; private set; }
+        public static void Terms(Action method)
+        {
+            InputString = null;
+            //Пользователь может удалять слова пока не введет exit или Enter
+            while (InputString != "exit" || InputString != "")
+            {
+                InputString = Console.ReadLine();
+                //Если пользователь ввел в консоль exit в любом регистре или нажал Enter - выйти из метода удаления
+                if (InputString.ToLower() == "exit" || InputString == "")
+                {
+                    return;
+                }
+                method();
+            }
+        }
+    }
+}
