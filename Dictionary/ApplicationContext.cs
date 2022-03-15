50b902f8b593dfd85f0cb89b3b3de4b7cb4b7b45
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2022-03-15 13:10:30 +0300

    Добавил асинхронную загрузку слов из бд
    Свойство GetEngWordsTask содержит задачу, которая асинхронно
    получает данные из бд. GetEngWords() возвращает загруженные данные из
    бд. GetEngWordsAsync() асинхронная версия этого метода.
    
    Обновил метод класса ShowWords, переменная words получает результат из
    задачи.

--- Dictionary/ApplicationContext.cs
+++ Dictionary/ApplicationContext.cs
@@ -26,5 +26,23 @@ namespace Dictionary
             modelBuilder.Entity<ForgottenEngWord>().Property(u => u.CountOfRepetitions).HasDefaultValue(3);
             modelBuilder.Entity<ForgottenEngWord>().HasCheckConstraint("CountOfRepetitions", "CountOfRepetitions >= 0 AND CountOfRepetitions <= 3");
         }
+        //свойство в котором будет находиться задача, которая получает все английские слова и их перевод из бд
+        public static Task<List<EngWord>> GetEngWordsTask { get; set; }
+        //асинхронный метод запускает задачу, которая получает все английские слова и их переводы из бд
+        async public static Task<List<EngWord>> GetEngWordsAsync()
+        {
+            List<EngWord> engWords = await Task<List<EngWord>>.Run(() => GetEngWords());
+            return engWords;
+        }
+        //метод получает все английские слова и их переводы из бaд
+        public static List<EngWord> GetEngWords()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                //получим все английские слова и их переводы
+                var engWordsList = db.EngWords.Include(w => w.OtherWords).ToList();
+                return engWordsList;
+            }
+        }
     }
 }
